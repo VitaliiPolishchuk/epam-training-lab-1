5a86b3312bd4ab8e286058aae082b7f41a0ea74a
Vitaliy Polischuk <vitaliy0polischuk@gmail.com>
2019-10-23 21:00:46 +0300

    09 dao

---
+++ src/main/java/the/best/dao/Dao.java
@@ -0,0 +1,19 @@
+package the.best.dao;
+
+import the.best.model.Room;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Dao<T> {
+
+    T get(long id);
+
+    List<T> getAll();
+
+    void save(T t);
+
+    void update(T t);
+
+    void delete(T t);
+}
